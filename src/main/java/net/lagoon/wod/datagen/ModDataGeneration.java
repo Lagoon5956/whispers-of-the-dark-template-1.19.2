58a9bc085bbaad1a0e696e49724b3773d1b3ddc6
Lagoon5956 <stevendevaney1503@gmail.com>
2025-05-25 02:14:30 +0100

    Added new datagen files woooo it now FUCKING WORKS

--- src/main/java/net/lagoon/wod/datagen/ModDataGeneration.java
+++ src/main/java/net/lagoon/wod/datagen/ModDataGeneration.java
@@ -5,7 +5,8 @@ import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
 
 public class ModDataGeneration implements DataGeneratorEntrypoint {
     @Override
-    public void onInitializeDataGenerator(FabricDataGenerator generator) {
-        generator.addProvider(new ModModelProvider(generator));
+    public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+        fabricDataGenerator.addProvider(ModModelProvider::new);
+
     }
 }
